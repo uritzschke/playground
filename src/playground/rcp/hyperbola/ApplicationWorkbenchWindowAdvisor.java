1e85ccc80ad4a3aa002717f0710f8e4b0f948de5
Uwe Ritzschke <uwe@andersanders.de>
2017-10-08 16:00:24 +0200

    Chapter 6.2 Adding a Status Line

--- src/playground/rcp/hyperbola/ApplicationWorkbenchWindowAdvisor.java
+++ src/playground/rcp/hyperbola/ApplicationWorkbenchWindowAdvisor.java
@@ -1,12 +1,17 @@
 package playground.rcp.hyperbola;
 
+import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
+
+	Image statusImage;
 
 	public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
 		super(configurer);
@@ -19,12 +24,25 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 	}
 
 	@Override
+	public void dispose() {
+		statusImage.dispose();
+	}
+
+	@Override
+	public void postWindowOpen() {
+		statusImage = AbstractUIPlugin.imageDescriptorFromPlugin("playground.rcp.hyperbola", IImageKeys.ONLINE)
+				.createImage();
+		final IStatusLineManager statusline = getWindowConfigurer().getActionBarConfigurer().getStatusLineManager();
+		statusline.setMessage(statusImage, "Online");
+	}
+
+	@Override
 	public void preWindowOpen() {
 		final IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
 		configurer.setInitialSize(new Point(640, 480));
 		configurer.setShowMenuBar(true);
 		configurer.setShowCoolBar(true);
-		configurer.setShowStatusLine(false);
+		configurer.setShowStatusLine(true);
 		configurer.setTitle("Hyperbola"); //$NON-NLS-1$
 	}
 }
